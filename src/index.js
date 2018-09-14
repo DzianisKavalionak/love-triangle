807303a94b5f139c284d755a82665d92e278319a
DzianisKavalionak <nereidy4@gmail.com>
2018-09-14 11:41:51 +0300

    .

--- src/index.js
+++ src/index.js
@@ -11,10 +11,10 @@ for (var a = 0; a < preferences.length; ++a) {
     for (var b = 0; b < preferences.length; ++b) {
         for (var c = 0; c < preferences.length; ++c) {
 
-            if (a === preferences[b] && b === preferences[c] && c === preferences[a]) {
+            if (a === preferences[b] && b === preferences[c] && c === preferences[a] && preferences[a] !== a && preferences[b] !== b && preferences[c] !== c) {
 
                 s = (s + +1);
-                console.log(a, b, c);
+                
             };
         };
     };
