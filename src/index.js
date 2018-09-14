b0f65d802abd11d677d53556cb959e44995127de
DzianisKavalionak <nereidy4@gmail.com>
2018-09-14 11:24:29 +0300

    .

--- src/index.js
+++ src/index.js
@@ -3,5 +3,22 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+
+
+var s = 0;
+preferences.unshift('noname');
+for (var a = 0; a < preferences.length; ++a) {
+    for (var b = 0; b < preferences.length; ++b) {
+        for (var c = 0; c < preferences.length; ++c) {
+
+            if (a === preferences[b] && b === preferences[c] && c === preferences[a]) {
+
+                s = (s + +1);
+                console.log(a, b, c);
+            };
+        };
+    };
+};
+s = s / 3;
+return s;
 };
